9b2b76740e129ddb2745352a24805a656e8c9dc0
deliadominte <domintedelia@yahoo.com>
2019-01-25 14:16:41 +0200

    message implementation
    implementation

--- public/js/newmsg.js
+++ public/js/newmsg.js
@@ -13,7 +13,14 @@ window.onload = () => {
                 const image = user.image;
 
                 document.getElementById('btn').onclick = e => {
+                    var currentdate = new Date(); 
+                    var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes();
                     const message = {
+                        time:datetime,
                         to: document.getElementById('to').value,
                         from: name,
                         createdBy: userId,
@@ -26,16 +33,30 @@ window.onload = () => {
 
                     db.collection('users').doc(userId).set(user, {merge: true});
 
-                    db.collection('users').where('username', '==', message.to).get().then(querySnapshot => {
+                    db.collection('users').where('username', '==', message.to).get().then(async querySnapshot => {
+                        let id_to;
+                        let notif;
                         querySnapshot.forEach(data => {
                             const receiver = data.data();
 
                             let em = receiver.received;
 
+                            id_to=data.id;
+
                             em.push(message);
+                            notif = {
+                                created: firebase.firestore.FieldValue.serverTimestamp(),
+                                seen: false,
+                                userId: id_to,
+                                createdBy: name,
+                                message: `<a href="/profile_for_others.html?userId=${userId}">${name}</a> sent you a <a href="/imbox.html">Message</a>`
+                            }
 
                             db.collection('users').doc(data.id).set(receiver, {merge: true});
                         });            
+                        await db.collection('notifications').add({ ...notif });
+                        
+                        window.location.href = '/sent.html';
                     });
                 }
             }
--- public/js/sent.js
+++ public/js/sent.js
@@ -3,24 +3,24 @@ window.onload = () => {
 
     if (userId) {
         db.collection('users').doc(userId).get().then(doc => {
-            let msg_rec=[];
+            let msg_snt=[];
             if (doc.exists) {
                 const user = doc.data();
 
-                msg_rec=user.received;
+                msg_snt=user.sent;
                 
             }
-            for (let index = 0; index < msg_rec.length; index++){
-                let msg=msg_rec[index];
+            for (let index = 0; index < msg_snt.length; index++){
+                let msg=msg_snt[index];
                 
-                let receiver;
-                db.collection('users').where('username', '==', msg.from).get().then( querySnapshot => {
+                let sent_to;
+                db.collection('users').where('username', '==', msg.to).get().then( querySnapshot => {
                     querySnapshot.forEach(data => {
-                         receiver = data.data();
+                         sent_to = data.data();
                          document.getElementById("messages").innerHTML += `
                          <div class="msg">
-                         <img src="${msg.image}" alt="Avatar">
-                         <a class="name" href="/profile_for_others.html?userId=${receiver.id}">${receiver.name}</a>
+                         <img src="${sent_to.image}" alt="Avatar">
+                         <a class="name" href="/profile_for_others.html?userId=${sent_to.id}">${sent_to.name}</a>
                          <br>
                          <p>Subject: </p><a class="subject">${msg.subject}</a>
                          <p>Message: </p><a class="subject">${msg.message}</a>
